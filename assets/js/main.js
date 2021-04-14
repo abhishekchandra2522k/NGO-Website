c8821f0313a4afddc27e9469cdeb060c080ae879
abhishekchandra2522k <1ac23456789@gmail.com>
2021-04-14 22:31:09 +0530

    Remove previous js, add new js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,4 @@
-// ==== Show Menu ==========
-const showMenu = (toggleId, navId) =>{
-    const toggle = document.getElementById(toggleId),
-    nav = document.getElementById(navId)
-
-
-    // Validate the variable exits
-    if(toggleId && nav){
-        toggle.addEventListener('click',()=>{
-            // We add the show-menu class to the div tag
-            nav.classList.toggle('show-menu')
-        })
-    }
-}
-
-showMenu('nav-toggle','nav-menu')
+window.addEventListener("scroll", function(){
+    var header = document.querySelector("header");
+    header.classList.toggle("sticky", window.scrollY > 0);
+});
