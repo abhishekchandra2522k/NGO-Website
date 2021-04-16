a41ca2018259ed4c4707b98851f8f135e82a8fa4
abhishekchandra2522k <1ac23456789@gmail.com>
2021-04-16 17:37:17 +0530

    Modify main.js, add diamond script

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,3 +2,11 @@ window.addEventListener("scroll", function(){
     var header = document.querySelector("header");
     header.classList.toggle("sticky", window.scrollY > 0);
 });
+
+$(".diamond-grid").diamonds({
+    size : 400, // Size of diamonds in pixels. Both width and height. 
+    gap : 10, // Pixels between each square.
+    hideIncompleteRow : false, // Hide last row if there are not enough items to fill it completely.
+    autoRedraw : true, // Auto redraw diamonds when it detects resizing.
+    itemSelector : ".item" // the css selector to use to select diamonds-items.
+});
